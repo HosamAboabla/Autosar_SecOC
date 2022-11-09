3a15d5bd7c3acd2cb9da46c6f60df7f2c342e8f2
Galal <galalnasser5@gmail.com>
2022-11-09 19:22:31 +0200

    Update SecOC_Types.h
    "implement the common types of SecOCRxPduProcessing"

--- include/SecOC_Types.h
+++ include/SecOC_Types.h
@@ -3,4 +3,31 @@
 
 #include "ComStack_Types.h"
 
+typedef enum
+{
+	BOTH,
+	FAILURE_ONLY,
+	NONE
+}SecOCClientServerVerificationStatusPropagationModeType;
+
+typedef enum
+{
+	QUEUE,
+	REJECT,
+	REPLACE
+}SecOCReceptionOverflowStrategyType;
+
+typedef enum
+{
+	BOTH,
+	FAILURE_ONLY,
+	NONE
+}SecOCVerificationStatusPropagationModeType;
+
+typedef enum
+{
+	SECOC_IFPDU,
+	SECOC_TPPDU
+}SecOCPdu_type;
+
 #endif
