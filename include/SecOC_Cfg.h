fafc2a67de6114c1fd20b28d7ac545e2d2ae72d1
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-11-11 22:56:52 +0200

    Changed comment style to square

--- include/SecOC_Cfg.h
+++ include/SecOC_Cfg.h
@@ -1,18 +1,39 @@
 #ifndef INCLUDE_SECOC_CFG_H_
 #define INCLUDE_SECOC_CFG_H_
 
-/*
- * Configuration parameter for SecOC_TxAuthenticPduLayer
- */
+/**************************************************************
+ *          * Container Info *                                *
+ *                                                            *
+ * Container_Name        : SecOC_TxAuthenticPduLayer          *
+ * Parent_Container_Name : SecOC_TxPduProcessing              *
+ * Container_Index       : 10.1.14                            *
+ **************************************************************/
+
 #define SECOC_PDU_TYPE                      ((SecOC_PduType_Type)SECOC_TPPDU)
 #define SECOC_TX_AUTHENTIC_LAYER_PDUID      ((uint16)0)
 
 
-/*
- * Configuration parameter for SecOC_TxSecuredPdu
- */
+/**************************************************************
+ *          * Container Info *                                *
+ *                                                            *
+ * Container_Name        : SecOC_TxSecuredPdu                 *
+ * Parent_Container_Name : SecOC_TxSecuredPduLayer             *
+ * Container_Index       : 10.1.16                            *
+ **************************************************************/
+
 #define SECOC_AUTH_PDU_HEADER_LENGTH        ((uint8)0)
 #define SECOC_TX_SECURED_LAYER_PDUID        ((uint16)0)
+
+/************************************************************
+ *          * Container Info *                              *
+ *                                                          *
+ * Container_Name        : SecOC_UseMessageLink             *
+ * Parent_Container_Name : SecOC_TxSecuredPduCollection     *
+ * Container_Index       : 10.1.20                          *
+ ***********************************************************/
+
+#define SECOC_MESSAGE_LINKLEN               ((uint16)0)
+#define SECOC_MESSAGE_LINKPOS               ((uint16)0)
 
 
 #endif
