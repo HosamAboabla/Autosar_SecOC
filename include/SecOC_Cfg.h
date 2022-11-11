7ac1696bf7ebcb991da17ac1ad1b7be7ec9176fb
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-11-11 15:48:44 +0200

    Edited comments

--- include/SecOC_Cfg.h
+++ include/SecOC_Cfg.h
@@ -2,14 +2,14 @@
 #define INCLUDE_SECOC_CFG_H_
 
 /*
- * Configuration parameter for the Pdu that is received by the SecOC module from the PduR
+ * Configuration parameter for SecOC_TxAuthenticPduLayer
  */
 #define SECOC_PDU_TYPE                      ((SecOC_PduType_Type)SECOC_TPPDU)
 #define SECOC_TX_AUTHENTIC_LAYER_PDUID      ((uint16)0)
 
 
 /*
- * Configuration parameter for the one Pdu that is transmitted by the SecOC module to the PduR after the Mac was generated
+ * Configuration parameter for SecOC_TxSecuredPdu
  */
 #define SECOC_AUTH_PDU_HEADER_LENGTH        ((uint8)0)
 #define SECOC_TX_SECURED_LAYER_PDUID        ((uint16)0)
