eb4b7d6f87e274b288497288bd603ee70a23d172
HusseinElsaka <ehussein366@gmail.com>
2022-11-11 21:54:06 +0200

    update Configration

--- include/SecOC_Lcfg.h
+++ include/SecOC_Lcfg.h
@@ -2,15 +2,9 @@
 #define INCLUDE_SECOC_LCFG_H_
 
 
-/*- INCLUDES -----------------------------------------------*/
 #include "Std_Types.h"
 #include "Rte_SecOC.h"
-/*- CONSTANTS ----------------------------------------------*/
-/*- PRIMITIVE TYPES ----------------------------------------*/
-/*- ENUMS --------------------------------------------------*/
-/*- STRUCTS AND UNIONS -------------------------------------*/
-/*- FUNCTION-LIKE MACROS -----------------------------------*/
-/*- FUNCTION DECLARATIONS ----------------------------------*/
+
 
 
 
@@ -19,20 +13,17 @@
 /*************************************************************
 *                          Container                         *
 * Name : SecOCMainFunctionTx                                 *
-* Description : Each element of this container defines one   *
-* instance of SecOC_MainFunctionTx.                          *
 * parent : SecOC                                             *
 * Index : 10.1.4                                             *
 **************************************************************/
 
-/***NOT SURE ABOUT THAT TYPE*/
-typedef uint16              SecOC_MainFunctionTxPartitionRefType;
+typedef uint16              SecOC_MainFunctionTxPartitionRefType;/* NOT SURE ABOUT THAT TYPE */
 
 
 typedef struct 
 {
     float64 SecOCMainFunctionPeriodTx;
-    SecOC_MainFunctionTxPartitionRefType *SecOCMainFunctionTxPartitionRef;
+    SecOC_MainFunctionTxPartitionRefType *SecOCMainFunctionTxPartitionRef; /* NOT SURE ABOUT THAT TYPE */
 
 }SecOC_MainFunctionTxType;
 
@@ -44,21 +35,16 @@ typedef struct
 /*************************************************************
 *                          Container                         *
 * Name : SecOCMainFunctionRx                                 *
-* Description : Each element of this container defines one   *
-* instance of SecOC_MainFunctionRx.                          *
 * parent : SecOC                                             *
 * Index : 10.1.3                                             *
 **************************************************************/
 
-
-
-/***NOT SURE ABOUT THAT TYPE*/
-typedef uint16              SecOC_MainFunctionRxPartitionRefType;
+typedef uint16              SecOC_MainFunctionRxPartitionRefType; /* NOT SURE ABOUT THAT TYPE */
 
 typedef struct 
 {
     float64 SecOCMainFunctionPeriodRx;
-    SecOC_MainFunctionRxPartitionRefType *SecOCMainFunctionRxPartitionRef;
+    SecOC_MainFunctionRxPartitionRefType *SecOCMainFunctionRxPartitionRef; /* NOT SURE ABOUT THAT TYPE */
 
 }SecOC_MainFunctionRxType;
 
@@ -68,12 +54,13 @@ typedef struct
 
 
 
-
-// /*container ->dont know where is it*/
-// typedef struct
-// {
+/*
+/*container ->dont know where is it
+typedef struct
+{
     
-// }SecOC_SecurityEventRefsType;
+}SecOC_SecurityEventRefsType;
+*/
 
 typedef enum {
     CFUNC,
@@ -83,11 +70,7 @@ typedef enum {
 
 
 
-
-
-/*callout function --> what should i do*/
-
-
+/* callout function --> what should i do */
 typedef void (*SecOC_VerificationStatusCalloutType) (SecOC_VerificationStatusType verificationStatus);
 
 
@@ -96,8 +79,6 @@ typedef void (*SecOC_VerificationStatusCalloutType) (SecOC_VerificationStatusTyp
 /*************************************************************
 *                          Container                         *
 * Name : SecOCGeneral                                        *
-* Description : Contains the general configuration           *
-* parameters of the SecOC module.                            *
 * parent : SecOC                                             *
 * Index : 10.1.2                                             *
 **************************************************************/
@@ -110,7 +91,6 @@ typedef struct
     boolean SecOCEnableForcedPassOverride;
     boolean SecOCEnableSecurityEventReporting;
     boolean SecOCIgnoreVerificationResult;
-    
     uint8 SecOCMaxAlignScalarType[100];  /* This type can be e.g. uint8, uint16 or uint32.*/
     boolean SecOCOverrideStatusWithDataId;
     boolean SecOCPropagateOnlyFinalVerificationStatus;
