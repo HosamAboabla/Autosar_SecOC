dc748b2491188dab7d28ea95ef2d1b78889d32e6
HusseinElsaka <ehussein366@gmail.com>
2022-11-12 10:04:07 +0200

    Update the indentation

--- include/SecOC_Cfg.h
+++ include/SecOC_Cfg.h
@@ -12,12 +12,12 @@
 /*
 * Configration parameter for SecOCMainFunctionPeriodTx
 */
-#define SECOC_MAIN_FUNCTION_PERIOD_TX                       ((float64)0)    
+#define SECOC_MAIN_FUNCTION_PERIOD_TX                                   ((float64)0)    
 
 /*
 * Configration parameter for SecOCMainFunctionTxPartitionRef
 */
-#define SECOC_MAIN_FUNCTION_TX_PARTITION_REF                ((SecOC_MainFunctionTxPartitionRefType)NULL)  
+#define SECOC_MAIN_FUNCTION_TX_PARTITION_REF                            ((SecOC_MainFunctionTxPartitionRefType)NULL)  
 
 
 
@@ -32,12 +32,12 @@
 /*
 * Configration parameter for SecOCMainFunctionPeriodRx
 */
-#define SECOC_MAIN_FUNCTION_PERIOD_RX                       ((float64)0)    
+#define SECOC_MAIN_FUNCTION_PERIOD_RX                                   ((float64)0)    
 
 /*
 * Configration parameter for SecOCMainFunctionRxPartitionRef
 */
-#define SECOC_MAIN_FUNCTION_RX_PARTITION_REF                ((SecOC_MainFunctionRxPartitionRefType)NULL)    
+#define SECOC_MAIN_FUNCTION_RX_PARTITION_REF                            ((SecOC_MainFunctionRxPartitionRefType)NULL)    
 
 
 
@@ -52,72 +52,72 @@
 /*
 * Configration parameter for SecOCDefaultAuthenticationInformationPatternMultiplicity
 */
-#define SECOC_DEFAULT_AUTHENTICATION_INFORMATION_PATTERN_MULTIPLICITY     ((uint8)0)      
+#define SECOC_DEFAULT_AUTHENTICATION_INFORMATION_PATTERN_MULTIPLICITY    ((uint8)0)      
  
  /*
 * Configration parameter for SecOCDefaultAuthenticationInformationPatternValue
 */
-#define SECOC_DEFAULT_AUTHENTICATION_INFORMATION_PATTERN_VALUE     ((uint8)0)
+#define SECOC_DEFAULT_AUTHENTICATION_INFORMATION_PATTERN_VALUE           ((uint8)0)
  
 /* 
 * Configration parameter for SecOCDevErrorDetect 
 */ 
-#define SECOC_DEV_ERROR_DETECT                               ((boolean)FALSE)
+#define SECOC_DEV_ERROR_DETECT                                           ((boolean)FALSE)
 
 
 /* 
 * Configration parameter for SecOCEnableForcedPassOverride 
 */ 
-#define SECOC_ENABLE_FORCED_PASS_OVERRIDE                    ((boolean)FALSE)     
+#define SECOC_ENABLE_FORCED_PASS_OVERRIDE                                ((boolean)FALSE)     
  
 
 /* 
 * Configration parameter for SecOCEnableSecurityEventReporting 
 */ 
-#define SECOC_ENABLE_SECURITY_EVENT_REPORTING                 ((boolean)FALSE)   
+#define SECOC_ENABLE_SECURITY_EVENT_REPORTING                            ((boolean)FALSE)   
 
 
 /* 
 * Configration parameter for SecOCIgnoreVerificationResult 
 */ 
-#define SECOC_IGNORE_VERIFICATION_RESULT                       ((boolean)FALSE)
+#define SECOC_IGNORE_VERIFICATION_RESULT                                 ((boolean)FALSE)
 
 
 /* 
 * Configration parameter for SecOCMaxAlignScalarType 
 */ 
-#define SECOC_MAX_ALIGN_SCALAR_TYPE                             ((uint8)0)
+#define SECOC_MAX_ALIGN_SCALAR_TYPE                                      ((uint8)0)
 
 
 /* 
 * Configration parameter for SecOCOverrideStatusWithDataId 
 */ 
-#define SECOC_OVERRIDE_STATUS_WITH_DATA_ID                      ((boolean)FALSE)
+#define SECOC_OVERRIDE_STATUS_WITH_DATA_ID                              ((boolean)FALSE)
 
 
 /* 
 * Configration parameter for SecOCPropagateOnlyFinalVerificationStatus 
 */ 
-#define SECOC_PROPAGATE_ONLY_FINAL_VERIFICATION_STATUS          ((boolean)FALSE)
+#define SECOC_PROPAGATE_ONLY_FINAL_VERIFICATION_STATUS                   ((boolean)FALSE)
 
 
 /* 
 * Configration parameter for SecOCQueryFreshnessValue 
 */ 
-#define SECOC_QUERY_FRESHNESS_VALUE                             ((SecOC_QueryFreshnessValueType)CFUNC)
+#define SECOC_QUERY_FRESHNESS_VALUE                                     ((SecOC_QueryFreshnessValueType)CFUNC)
 
 
 /* 
 * Configration parameter for SecOCVerificationStatusCallout 
 */ 
-#define SECOC_VERIFICATION_STATUS_CALLOUT                       ((SecOC_VerificationStatusCalloutType)NULL) /* Here a address for function */
+#define SECOC_VERIFICATION_STATUS_CALLOUT                               ((SecOC_VerificationStatusCalloutType)NULL) /* Here a address for function */
 
 
 
 /* 
 * Configration parameter for SecOCVersionInfoApi 
 */ 
-#define SECOC_VERSION_INFO_API                                  ((boolean)FALSE)
+#define SECOC_VERSION_INFO_API                                          ((boolean)FALSE)
 
 
 
