b2862daa18d0dcc340dd100da0d75f37e1e703ca
Hosam Aboabla <hosamaboabla@yahoo.com>
2022-12-01 19:20:13 +0200

    add SecOC_IfTransmit implementation

--- source/SecOC.c
+++ source/SecOC.c
@@ -4,3 +4,20 @@
 #include "Det.h"
 #include "Rte_SecOC.h"
 #include "SecOC.h"
+
+Pdu_InfoType* SecOC_Buffer[SECOC_BUFFERLENGTH];
+
+Std_ReturnType SecOC_IfTransmit( Pdu_IdType TxPduId, const Pdu_InfoType* PduInfoPtr)
+{
+    Std_ReturnType result = E_OK; 
+    if( TxPduId < SECOC_BUFFERLENGTH )
+    {
+        SecOC_Buffer[TxPduId] = PduInfoPtr;
+    }
+    else
+    {
+        result = E_NOT_OK;
+    }
+
+    return result;
+}
