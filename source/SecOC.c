92ca73f5dae2f229badeeac1d0f3b4428596c163
Hosam Aboabla <hosamaboabla@yahoo.com>
2022-12-02 13:14:47 +0200

    update SecOC_Buffer removing pointer

--- source/SecOC.c
+++ source/SecOC.c
@@ -5,14 +5,14 @@
 #include "Rte_SecOC.h"
 #include "SecOC.h"
 
-Pdu_InfoType* SecOC_Buffer[SECOC_BUFFERLENGTH];
+Pdu_InfoType SecOC_Buffer[SECOC_BUFFERLENGTH];
 
 Std_ReturnType SecOC_IfTransmit( Pdu_IdType TxPduId, const Pdu_InfoType* PduInfoPtr)
 {
     Std_ReturnType result = E_OK; 
     if( TxPduId < SECOC_BUFFERLENGTH )
     {
-        SecOC_Buffer[TxPduId] = PduInfoPtr;
+        SecOC_Buffer[TxPduId] = *PduInfoPtr;
     }
     else
     {
