5f05647046d3de57f072559d8a4ca821946413de
Hosam Aboabla <hosamaboabla@yahoo.com>
2022-12-06 19:06:58 +0200

    add SecOC_MainFunctionTx

--- source/SecOC.c
+++ source/SecOC.c
@@ -5,12 +5,12 @@
 #include "Det.h"
 #include "Rte_SecOC.h"
 #include "SecOC.h"
+#include "PduR_SecOC.h"
 
 
 
 
-
-PduInfoType SecOC_Buffer[SECOC_BUFFERLENGTH];
+PduInfoType SecOC_Buffer[SECOC_BUFFERLENGTH] = { NULL };
 
 Std_ReturnType SecOC_IfTransmit( PduIdType TxPduId, const PduInfoType* PduInfoPtr)
 {
@@ -51,3 +51,39 @@ void SecOC_TxConfirmation (PduIdType TxPduId,Std_ReturnType result)
 
 }
 
+
+
+
+
+/****************************************************
+ *          * Function Info *                           *
+ *                                                      *
+ * Function_Name        : SecOC_MainFunctionTx          *
+ * Function_Index       : 8.6.2                         *
+ * Function_File        : SWS of secOC                  *
+ * Function_Descripton  : The lower layer communication * 
+ * interface module confirms  the transmission of a PDU *
+ *        or the failure to transmit a PDU              *
+ ***************************************************/
+void SecOC_MainFunctionTx(void)
+{
+    // check if initialized or not;
+    PduIdType idx = 0;
+    PduInfoType transmitPduInfo;
+    for( ; idx < SECOC_BUFFERLENGTH ; idx++)
+    {
+        // check if there is data
+        if( SecOC_Buffer[idx] != NULL )
+        {
+            // authenticate SecOC_Buffer[idx];
+            // send authenticated data
+            // authenticate( SecOC_Buffer[idx] , &transmitPduInfo)
+            PduR_SecOCTransmit(idx , &transmitPduInfo);
+
+        }
+        else
+        {
+
+        }
+    }
+}
