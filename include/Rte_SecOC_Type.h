63e9f8d5b02710da8e2474d40085c1c1be5efbb4
HusseinElsaka <ehussein366@gmail.com>
2022-12-08 15:43:23 +0200

    adding new type
    Freshness array type that array hold the freshness value

---
+++ include/Rte_SecOC_Type.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_RTE_SECOC_TYPES_H_
+#define INCLUDE_RTE_SECOC_TYPES_H_
+
+#include "Std_Types.h"
+
+#define SECOC_MAX_FRESHNESS_SIZE 100
+
+typedef uint8                 SecOC_FreshnessArrayType[SECOC_MAX_FRESHNESS_SIZE];
+
+#endif
