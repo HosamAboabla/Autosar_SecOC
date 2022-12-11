f7831b77caf9f55d5c950a9f9eafa903898c1142
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-12-11 10:55:22 +0200

    Added initial shape of authenticate

--- source/SecOC.c
+++ source/SecOC.c
@@ -9,10 +9,57 @@
 #include "PduR_SecOC.h"
 #include "Csm.h"
 
+#include <string.h>
 
 
 PduInfoType SecOC_Buffer[SECOC_BUFFERLENGTH];
 
+
+/****************************************************
+ *          * Function Info *                           *
+ *                                                      *
+ * Function_Name        : authenticate                  *
+ * Function_Index       : SecOC internal                *
+ * Function_File        : SWS of secOC                  *
+ * Function_Descripton  : The lower layer communication * 
+ * interface module confirms  the transmission of a PDU *
+ *        or the failure to transmit a PDU              *
+ ***************************************************/
+//        authenticate( SecOC_Buffer[idx] , &transmitPduInfo)
+authenticate(const PduIdType TxPduId, const PduInfoType* AuthPdu, PduInfoType* SecPdu)
+/*Std_ReturnType authenticate(
+    const PduIdType TxPduId, 
+    const PduInfoType* PduInfoPtr,
+    uint8* authPtr,
+    uint32* authLengthPtr)*/
+{
+    // 1. Prepare Secured I-PDU
+    // 2. Construct Data for Authenticator
+    // DataToAuthenticator = Data Identifier | secured part of the Authentic I-PDU | Complete Freshness Value
+    uint8 DataToAuth[100]; // Discuss size later
+    uint8 DataToAuthLen = 0;
+
+    // Data Identifier
+    memcpy(&DataToAuth[DataToAuthLen], TxPduId, sizeof(TxPduId));
+    DataToAuthLen += sizeof(TxPduId);
+
+    // secured part of the Authentic I-PDU
+    memcpy(&DataToAuth[DataToAuthLen], AuthPdu->SduDataPtr, AuthPdu->SduLength);
+    DataToAuthLen += AuthPdu->SduLength;
+
+    // Complete Freshness Value
+    /*
+    memcpy(&DataToAuth[DataToAuthLen], PduInfoPtr->SduDataPtr, PduInfoPtr->SduLength);
+    DataToAuthLen += PduInfoPtr->SduLength;
+    */
+
+    Std_ReturnType result;
+    uint8*  authenticatorPtr;
+    uint32  authenticatorLen;
+    result = Csm_MacGenerate(TxPduId, 0, DataToAuth, DataToAuthLen, authenticatorPtr, &authenticatorLen);
+
+    return result;
+}
 
 extern void SecOC_MainFunctionTx ( void )
 {
