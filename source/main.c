14632655e1499a2b5a3a7dfef1e92a734ba759d7
Hosam Aboabla <hosamaboabla@yahoo.com>
2022-12-13 13:21:45 +0200

    testing AES

--- source/main.c
+++ source/main.c
@@ -1,10 +1,22 @@
 
 #include "SecOC.h"
 #include <stdio.h>
+#include "encrypt.h"
 
 int main(void)
 {
     SecOC_MainFunctionTx();
+    uint32 data[16] = {'h','o','s','a' , 'm'};
+    uint8 mac[16];
+    uint32 macLen = 8;
+    startEncryption(data , 8 , mac ,&macLen);
+
+    printf("Encrypted message in hex:\n");
+    for (int i = 0; i < macLen ; i++) {
+        printf("%x ", mac[i]);
+    }
+    printf("\n");
+
 
     printf("Program ran successfully\n");
     return 0;
