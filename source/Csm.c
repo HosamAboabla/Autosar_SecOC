e51221226d239d2e743b15c5ee5e0354ab200db3
Galal <galalnasser5@gmail.com>
2022-12-16 01:34:01 +0200

    Update Csm.c

--- source/Csm.c
+++ source/Csm.c
@@ -24,12 +24,11 @@ extern Std_ReturnType Csm_MacGenerate (
         return E_OK;
     }
 
-#define MAC_LENGTH_BYTES 4
 #define MAX_MAC_BUFFER  32
 Std_ReturnType Csm_MacVerify(uint32 jobId, Crypto_OperationModeType mode, const uint8* dataPtr, uint32 dataLength,
 const uint8* macPtr, const uint32 macLength, Crypto_VerifyResultType* verifyPtr)
 {
-    uint32 Maclen = MAC_LENGTH_BYTES;
+    uint32 Maclen = BIT_TO_BYTES(macLength);
     uint8 Mac_from_data[MAX_MAC_BUFFER];
 
     Std_ReturnType Mac_status, result;
@@ -38,7 +37,6 @@ const uint8* macPtr, const uint32 macLength, Crypto_VerifyResultType* verifyPtr)
     
     if ((Mac_status == E_OK) && (Maclen == (BIT_TO_BYTES(macLength)))) 
     {
-        //printf("good");
         if ((memcmp(Mac_from_data, macPtr, Maclen)) == 0) 
         {
             result = E_OK;
