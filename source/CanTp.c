452380cc3c207970c2c0123da97116d129d873c6
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2023-02-12 13:48:50 +0200

    Comment out some lines of code at CanTp.c file

--- source/CanTp.c
+++ source/CanTp.c
@@ -14,17 +14,17 @@ Std_ReturnType CanTp_Transmit(PduIdType TxPduId,const PduInfoType* PduInfoPtr)
     result = ethernet_send(PduInfoPtr->SduDataPtr , PduInfoPtr->SduLength);
     #endif
 
-    PduR_CanTpTxConfirmation(TxPduId , result); /*should be called at the end of Hussein's Function*/
+    // PduR_CanTpTxConfirmation(TxPduId , result); /*should be called at the end of Hussein's Function*/
 
-    /*Also the code below should be called after Hussein's Function
-    * It may be with every transmission of the segmented data or when the transmission finishes
-    */
-    if(TP_TRANSMISSION_STATUS)
-    {
-        return E_OK;
-    }
-    else
-    {
-        return E_NOT_OK;
-    }
+    // /*Also the code below should be called after Hussein's Function
+    // * It may be with every transmission of the segmented data or when the transmission finishes
+    // */
+    // if(TP_TRANSMISSION_STATUS)
+    // {
+    //     return E_OK;
+    // }
+    // else
+    // {
+    //     return E_NOT_OK;
+    // }
 }
