06cb66fa75c60d3c9b9cf9711217531b55d41f15
Galal <67324703+galal10@users.noreply.github.com>
2023-02-16 19:41:13 +0200

    modify configs

--- include/SecOC_Cfg.h
+++ include/SecOC_Cfg.h
@@ -211,12 +211,12 @@
 /*
 * Configuration Parameter for SecOC_FreshnessValueLength
 */
-#define SECOC_RX_FRESHNESS_VALUE_LENGTH     ((uint8)16)
+#define SECOC_RX_FRESHNESS_VALUE_LENGTH     ((uint8)24)
 
 /*
 * Configuration Parameter for SecOC_FreshnessValueTruncLength
 */
-#define SECOC_RX_FRESHNESS_VALUE_TRUNCLENGTH        ((uint8)14)
+#define SECOC_RX_FRESHNESS_VALUE_TRUNCLENGTH        ((uint8)20)
 
 /*
 * Configuration Parameter for SecOC_ReceptionOverflowStrategy
@@ -443,10 +443,10 @@
 
 #define SECOC_AUTHENTICATION_BUILD_ATTEMPTS                         ((uint16) 5)
 #define SECOC_TX_AUTH_INFO_TRUNC_LENGTH                             ((uint16) 32)
-#define SECOC_TX_DATA_ID                                            ((uint16) 20)
+#define SECOC_TX_DATA_ID                                            ((uint16) 0)
 #define SECOC_TX_FRESHNESS_VALUE_ID                                 ((uint16) 10)
-#define SECOC_TX_FRESHNESS_VALUE_LENGTH                             ((uint8) 16)
-#define SECOC_TX_FRESHNESS_VALUE_TRUNC_LENGTH                       ((uint8 ) 14)
+#define SECOC_TX_FRESHNESS_VALUE_LENGTH                             ((uint8) 24)
+#define SECOC_TX_FRESHNESS_VALUE_TRUNC_LENGTH                       ((uint8 ) 20)
 #define SECOC_PROVIDE_TX_TRUNCATED_FRESHNESS_VALUE                  ((boolean) 1)
 #define SECOC_RE_AUTHENTICATE_AFTER_TRIGGER_TRANSMIT                ((boolean) 1)
 #define SECOC_TX_PDU_UNUSED_AREAS_DEFAULT                           ((uint8) 0)
