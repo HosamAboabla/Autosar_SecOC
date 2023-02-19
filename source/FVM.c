b7ec6cda30cc4f9c7de642c6420e05c6bb044dca
Galal <67324703+galal10@users.noreply.github.com>
2023-02-19 21:09:41 +0200

    fixed bug bytes instead of bits

--- source/FVM.c
+++ source/FVM.c
@@ -183,13 +183,13 @@ Std_ReturnType FVM_GetRxFreshness(uint16 SecOCFreshnessValueID, const uint8 *Sec
         /* verified that the constructed FreshnessVerifyValue is larger than the last stored notion of the Freshness Value */
         /* If it is not larger than the last stored notion of the Freshness Value,
          the FVM shall stop the verification and drop the Secured I-PDU */
-        if (memcmp(Freshness_Counter[SecOCFreshnessValueID], SecOCFreshnessValue , *SecOCFreshnessValueLength) < 0)
+        if (memcmp(Freshness_Counter[SecOCFreshnessValueID], SecOCFreshnessValue , BIT_TO_BYTES(*SecOCFreshnessValueLength)) < 0)
         {
-             result = E_OK;
+            result = E_OK;
         }
         else
         {
-             result= E_NOT_OK;
+            result= E_NOT_OK;
         }
 
     }   
