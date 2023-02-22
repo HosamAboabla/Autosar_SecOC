61e627bd63440767e8811f742c3b8243bb66c98d
Galal <67324703+galal10@users.noreply.github.com>
2023-02-22 20:53:11 +0200

    add CanTP_MainFunctionRx function

---
+++ include/CanTP.h
@@ -0,0 +1,12 @@
+#ifndef CAN_TP_H_
+#define CAN_TP_H_
+
+#include "ComStack_Types.h"
+#include "Std_Types.h"
+#define TP_LENGTH       4
+#define BUFFER_SIZE     200
+
+
+void CanTP_MainFunctionRx(void);
+
+#endif /* CAN_TP_H_ */
---
+++ source/CanTP.c
@@ -0,0 +1,8 @@
+#include "SecOC.h"
+#include "Pdur_CanTP.h"
+#include "CanTP.h"
+
+void CanTP_MainFunctionRx(void)
+{
+
+}
