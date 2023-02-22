b333bd51ea766ba18898f41f1119eee4188cdb34
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2023-02-22 17:42:18 +0200

    delete comments that are not needed in code

--- source/CanTp.c
+++ source/CanTp.c
@@ -13,18 +13,4 @@ Std_ReturnType CanTp_Transmit(PduIdType TxPduId,const PduInfoType* PduInfoPtr)
     #ifdef LINUX
     result = ethernet_send(PduInfoPtr->SduDataPtr , PduInfoPtr->SduLength);
     #endif
-
-    // PduR_CanTpTxConfirmation(TxPduId , result); /*should be called at the end of Hussein's Function*/
-
-    // /*Also the code below should be called after Hussein's Function
-    // * It may be with every transmission of the segmented data or when the transmission finishes
-    // */
-    // if(TP_TRANSMISSION_STATUS)
-    // {
-    //     return E_OK;
-    // }
-    // else
-    // {
-    //     return E_NOT_OK;
-    // }
 }
