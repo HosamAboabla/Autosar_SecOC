35ae06c26d5229539df6ed29512f2f46329c65ad
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2023-02-22 15:57:32 +0200

    add PduR_CanTpCopyTxData prototype and body

--- source/PduR_CanTp.c
+++ source/PduR_CanTp.c
@@ -1,7 +1,17 @@
-#include "PduR_CanTp.h"
+#include "Pdur_CanTP.h"
 #include "SecOC.h"
+
+BufReq_ReturnType PduR_CanTpCopyTxData (PduIdType id,const PduInfoType* info,const RetryInfoType* retry,
+PduLengthType* availableDataPtr)
+{
+   /* SWS_CanTp_00086 */
+   return SecOC_CopyTxData(id, info, retry, availableDataPtr);
+}
+
+
 
 void PduR_CanTpTxConfirmation(PduIdType TxPduId, Std_ReturnType result)
 {
-	SecOC_TpTxConfirmation( TxPduId,  result );
+    // forward result to SecOC
+    SecOC_TpTxConfirmation(TxPduId, result);
 }
