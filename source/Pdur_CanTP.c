71b581c2c538ab6be9e1678a9f5058061b719a25
Galal <67324703+galal10@users.noreply.github.com>
2023-02-22 17:10:46 +0200

    implement PduR_CanTpCopyRxData function

--- source/Pdur_CanTP.c
+++ source/Pdur_CanTP.c
@@ -15,3 +15,9 @@ void PduR_CanTpTxConfirmation(PduIdType TxPduId, Std_ReturnType result)
     // forward result to SecOC
     SecOC_TpTxConfirmation(TxPduId, result);
 }
+
+BufReq_ReturnType PduR_CanTpCopyRxData (PduIdType id,const PduInfoType* info,PduLengthType* bufferSizePtr)
+{
+    // SWS_PduR_00428 
+    //SecOC_CopyRxData(id, info, bufferSizePtr);
+}
