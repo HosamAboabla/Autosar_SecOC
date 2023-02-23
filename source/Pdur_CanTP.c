4312f9a8813398c62bdf2808dc521a95cd70e16a
Galal <67324703+galal10@users.noreply.github.com>
2023-02-23 18:01:37 +0200

    implement PduR_CanTpCopyRxData & PduR_CanTpStartOfReception function

--- source/Pdur_CanTP.c
+++ source/Pdur_CanTP.c
@@ -15,3 +15,15 @@ void PduR_CanTpTxConfirmation(PduIdType TxPduId, Std_ReturnType result)
     // SWS_PduR_00301
     SecOC_TpTxConfirmation(TxPduId, result);
 }
+
+BufReq_ReturnType PduR_CanTpCopyRxData (PduIdType id,const PduInfoType* info,PduLengthType* bufferSizePtr)
+{
+    // SWS_PduR_00428 
+    //SecOC_CopyRxData(id, info, bufferSizePtr);
+}
+
+BufReq_ReturnType PduR_CanTpStartOfReception(PduIdType id, const PduInfoType* info, PduLengthType TpSduLength, PduLengthType* bufferSizePtr)
+{
+    // SWS_PduR_00549
+    //SecOC_StartOfReception(id, info, TpSduLength, bufferSizePtr);
+}
