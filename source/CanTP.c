0ff607e516f4982eb2d1a9082c05462fa208a826
HusseinElsaka <ehussein366@gmail.com>
2023-02-26 19:46:35 +0200

    Main Rx
    Used here data i put
    for loop in every PDU see if will receive or not
    if e_ok send it with
    and send indiacation in last

--- source/CanTP.c
+++ source/CanTP.c
@@ -11,31 +11,45 @@ void CanTP_MainFunctionRx(void)
     
     PduInfoType TP_PDU;
     uint8 Meta_data = 0;
+    /* Here i recieve data */
+    uint8 ReciveDATA[] = {11,22,33,44,55,66,77,88,99,111,122,133,144,155,166};
+    TP_PDU.SduDataPtr = ReciveDATA;
     TP_PDU.MetaDataPtr = &Meta_data;
     TP_PDU.SduLength = 4;
-    PduLengthType TP_length = 100;
+    PduLengthType TP_length = 15;
     PduLengthType bufferSizePtr;
 
     BufReq_ReturnType Result;
-    
-    PduR_CanTpStartOfReception(idx, &TP_PDU,TP_length, &bufferSizePtr);
-
-    for( idx = 0; idx < BUFFER_SIZE; idx++)
-    {
-        for(int i=0; i < TP_length/TP_LENGTH; i++)
+    for(idx = 0; idx < SECOC_NUM_OF_RX_PDU_PROCESSING; idx++)
+    {  
+        /* Check if can Receive  */
+        if (PduR_CanTpStartOfReception(idx, &TP_PDU,TP_length, &bufferSizePtr) == BUFREQ_OK)
         {
-            Result = PduR_CanTpCopyRxData(idx, &TP_PDU, &bufferSizePtr);
-            if( Result != BUFREQ_OK)
-                break;
-        }
+            /* send Data */
+           for(int i = 0; i < TP_length/TP_LENGTH; i++)
+           {
+                Result = PduR_CanTpCopyRxData(idx, &TP_PDU, &bufferSizePtr);
+                if( Result == BUFREQ_OK)
+                {
+                    TP_PDU.SduDataPtr += TP_LENGTH;
+                }
+                else
+                {
+                    break;
+                }
+           }
 
-        if( Result != BUFREQ_OK)
-            continue;
+           if(Result != BUFREQ_OK)
+               continue;
+
+           if(TP_length % TP_LENGTH != 0)
+           {
+                TP_PDU.SduLength = TP_length % TP_LENGTH;
+                Result = PduR_CanTpCopyRxData(idx, &TP_PDU, &bufferSizePtr);
+           }
+           /* Send Confirm to last of data */
+           PduR_CanTpRxIndication(idx,Result);
+        }
         
-        if(CanTP_Buffer[idx].SduLength % TP_LENGTH !=0)
-        {
-            Result = PduR_CanTpCopyRxData(idx, &TP_PDU, &bufferSizePtr);
-        }
     }
-    //PduR_CanTpRxIndication(idx,Result);
 }
