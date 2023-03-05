7220233cfd199766d22bca2eff29345da4824a12
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2023-03-05 21:08:12 +0200

    add SecOC_Debug.h header file
    1- create the header file
    2- include stdio.h for printf statements
    3- define SECOC_DEBUG macro by 1 or 0
    
    This file is used for enabling or disabling debug macros in code.

---
+++ include/SecOC_Debug.h
@@ -0,0 +1,8 @@
+#ifndef SECOC_DEBUG_H
+#define SECOC_DEBUG_H
+
+#include <stdio.h>      /*in order to use printf statements for debugging*/
+
+#define SECOC_DEBUG     1       /*1: debugging mode, 0: deploy mode*/
+
+#endif
