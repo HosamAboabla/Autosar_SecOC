a1109a5927a4a36732619de2ed86e3b220636afe
HusseinElsaka <ehussein366@gmail.com>
2023-03-06 16:42:06 +0200

    Adding ID to each of Ethernet
    Send --to send it at last of frame
    Receive -- to revieve at pointer to use it

--- include/ethernet.h
+++ include/ethernet.h
@@ -10,9 +10,9 @@
 
 #define PORT_NUMBER 12345
 
-Std_ReturnType ethernet_send(unsigned char* data , unsigned char dataLen);
+Std_ReturnType ethernet_send(unsigned char id, unsigned char* data , unsigned char dataLen);
 
-Std_ReturnType ethernet_receive(unsigned char* data , unsigned char dataLen);
+Std_ReturnType ethernet_receive(unsigned char* data , unsigned char dataLen, unsigned char* id);
 
 
 
