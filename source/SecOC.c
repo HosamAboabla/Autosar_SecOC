6fc5a5793224404315ccebbf273fc80269605050
Galal <67324703+galal10@users.noreply.github.com>
2023-03-16 02:02:57 +0200

    remove test code

--- source/SecOC.c
+++ source/SecOC.c
@@ -370,7 +370,7 @@ void SecOCMainFunctionTx(void)
 
 
             /* [SWS_SecOC_00062] */
-            // PduR_SecOCTransmit(idx , securedPdu);
+            PduR_SecOCTransmit(idx , securedPdu);
             
 
         }
@@ -909,50 +909,6 @@ BufReq_ReturnType SecOC_CopyRxData (PduIdType id, const PduInfoType* info, PduLe
 #ifdef DEBUG_ALL
 extern SecOC_ConfigType SecOC_Config;
 void SecOC_test()
-{
-    // TX
-    for(int i = 0; i < 0x2fe; i++)
-       FVM_IncreaseCounter(9); 
-    // RX
-    for(int i = 0; i < 0x1ff; i++)
-       FVM_IncreaseCounter(10);
-    
-    uint8 buff[20]={10,100,200,250};
-    PduLengthType len = 4;
-    PduInfoType SPDU;
-    uint8 meta = 0;
-    SPDU.MetaDataPtr = &meta;
-    SPDU.SduDataPtr = buff;
-    SPDU.SduLength = len;
-
-    SecOC_Init(&SecOC_Config);
-    SecOC_IfTransmit(0, &SPDU);
-    
-    printf("Data transmit :\n");
-    
-    for(int i = 0; i < SPDU.SduLength; i++)
-        printf("%d ", SPDU.SduDataPtr[i]);
-    printf("\n");
- 
-    PduInfoType *secured_TX = &(SecOCTxPduProcessing[0].SecOCTxSecuredPduLayer->SecOCTxSecuredPdu->SecOCTxSecuredLayerPduRef);
-    PduInfoType *auth = &(SecOCTxPduProcessing[0].SecOCTxAuthenticPduLayer->SecOCTxAuthenticLayerPduRef);
-    PduInfoType *auth_RX = &(SecOCRxPduProcessing[0].SecOCRxAuthenticPduLayer->SecOCRxAuthenticLayerPduRef);
-
-    SecOCMainFunctionTx();
-    printf("\ndata after authen: \n");
-    for(int i = 0; i < secured_TX->SduLength; i++)
-        printf("%d ", secured_TX->SduDataPtr[i]);
-    printf("\ndone Tx\n");   
-
-    PduR_CanIfRxIndication((uint16)0,secured_TX);
-
-    PduInfoType *secured = &(SecOCRxPduProcessing[0].SecOCRxSecuredPduLayer->SecOCRxSecuredPdu->SecOCRxSecuredLayerPduRef);
-    
-    printf("in RX data received : \n");
-    for(int i = 0; i < secured->SduLength; i++)
-        printf("%d ", secured->SduDataPtr[i]);
-    printf("\n");  
-    // main function
-    SecOCMainFunctionRx();       
+{     
 }
 #endif
