7063a450e7fcbc0ba85d3e0d454a30e0cfc958d8
HusseinElsaka <ehussein366@gmail.com>
2023-03-17 15:47:40 +0200

    Update SecOC_Lcfg.c

--- source/SecOC_Lcfg.c
+++ source/SecOC_Lcfg.c
@@ -365,5 +365,10 @@ SecOC_RxCountersType SecOC_RxCounters[SECOC_NUM_OF_RX_PDU_PROCESSING] =
         /* [SWS_SecOC_00235] */
         .AuthenticationCounter = 0,
         .VerificationCounter = 0
+    },
+    {
+        /* [SWS_SecOC_00235] */
+        .AuthenticationCounter = 0,
+        .VerificationCounter = 0
     }
 };
