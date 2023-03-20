91fde3489b8254e360b63852eee4a6ac0fb56208
Galal <67324703+galal10@users.noreply.github.com>
2023-03-20 12:58:50 +0200

    add container SecOC_TxAuthenticPduLayer[1] fot Tp

--- source/SecOC_Lcfg.c
+++ source/SecOC_Lcfg.c
@@ -178,9 +178,14 @@ SecOC_TxAuthenticPduLayerType SecOC_TxAuthenticPduLayer[]=
         {AuthPdu0BufferTx, NULL, 0},
     },
     {   
-        SECOC_TX_PDUTYPE,
-        SECOC_TX_AUTHENTIC_LAYER_PDUID,
-        {AuthPdu1BufferTx, NULL, 0},
+        .SecOCPduType =                 SECOC_TPPDU,
+        .SecOCTxAuthenticLayerPduId =   ((uint16) 1),
+        .SecOCTxAuthenticLayerPduRef = 
+        {
+            .SduDataPtr =               AuthPdu1BufferTx,
+            .MetaDataPtr =              NULL,
+            .SduLength =                ((PduLengthType) 0)
+        }
     }
 };
 
