9c0b60a89a8c5ffea1511167939d224d495d6155
HusseinElsaka <ehussein366@gmail.com>
2023-03-22 15:08:31 +0200

    return the main function as it

--- source/main.c
+++ source/main.c
@@ -1,110 +1,12 @@
 
 #include "SecOC.h"
 #include "SecOC_Debug.h"
-#include <pthread.h>
-#include <stdlib.h>
 
-
-void* tx_com()
-{
-    while (1)
-    {
-        Com_MainTx();
-    }
-}
-
-void* tx_secoc()
-{
-    while (1)
-    {
-        SecOCMainFunctionTx();
-    }
-}
-
-void* tx_tp()
-{
-    while (1)
-    {
-        CanTp_MainFunction();
-    }
-
-}
-void* rx_ethernet()
-{
-    while (1)
-    {
-        //ethernet_RecieveMainFunction();
-    }
-}
-
-void* rx_tp()
-{
-    while (1)
-    {
-        CanTP_MainFunctionRx();
-    }
-    
-    
-}
-void* rx_secoc()
-{
-    while (1)
-    {
-         SecOCMainFunctionRx();
-    }
-}
-
-
-
-boolean create_thread (pthread_t *thread, void *fn() )
-{
-    if(pthread_create(thread , NULL, fn, NULL) != 0)
-    {
-        printf("error create thread");
-        return 1;
-    }
-    return 0;
-}
-
-boolean join_thread (pthread_t *thread)
-{
-    if(pthread_join(*thread,NULL))
-    {
-        printf("error join thread ");
-        return 1;
-    }
-    return 0;
-}
-
-
-extern SecOC_ConfigType SecOC_Config;
 int main(void)
 {
-    SecOC_Init(&SecOC_Config);
-    pthread_t t[6];
-    void *(*fn_p[6])() = {tx_com, tx_secoc, tx_tp, rx_ethernet, rx_tp, rx_secoc};
-
-    /* Creating Threads */
-    for (int i = 0; i < 6; i++)
-    {
-        if (create_thread(&t[i], fn_p[i]) != 0)
-        {
-            printf("%d \n", i);
-        }
-    }   
-
-
-    for (int i = 0; i < 6; i++)
-    {
-        if (join_thread(&t[i]) != 0)
-        {
-            printf("%d \n", i);
-        }
-    }   
-    // #ifdef DEBUG_ALL
-    //     SecOC_test();
-    // #endif
-
+    #ifdef DEBUG_ALL
+        SecOC_test();
+    #endif
     (void)printf("Program ran successfully\n");
     return 0;
 }
