3cde3d3e09b8beea61610926fd81756b6e3e86e8
Galal <67324703+galal10@users.noreply.github.com>
2023-03-23 13:42:01 +0200

    remove containers

--- include/SecOC/SecOC_Lcfg.h
+++ include/SecOC/SecOC_Lcfg.h
@@ -17,8 +17,8 @@
 
 #define SECOC_RX_DATA_TO_AUTHENTICATOR_LENGTH                       (sizeof(PduIdType) + SECOC_AUTHPDU_MAX_LENGTH + (SECOC_FRESHNESS_MAX_LENGTH/8 + 1))
 
-#define SECOC_NUM_OF_TX_PDU_PROCESSING                               (5)
-#define SECOC_NUM_OF_RX_PDU_PROCESSING                               (5)
+#define SECOC_NUM_OF_TX_PDU_PROCESSING                               (3)
+#define SECOC_NUM_OF_RX_PDU_PROCESSING                               (3)
 
 /* [SWS_SecOC_00057] The SecOC module shall provide sufficient buffers to store all intermediate data */
 typedef struct
