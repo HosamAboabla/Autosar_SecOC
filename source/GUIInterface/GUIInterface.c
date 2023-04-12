0af2b7bde28e054473cb410375d958b3f60927ca
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-04-13 01:32:29 +0200

    Added errorString, that returns string based on the stderror

--- source/GUIInterface/GUIInterface.c
+++ source/GUIInterface/GUIInterface.c
@@ -9,6 +9,25 @@ const SecOC_TxPduProcessingType     *SecOCTxPduProcessing;
 const SecOC_RxPduProcessingType     *SecOCRxPduProcessing;
 const SecOC_GeneralType             *SecOCGeneral;
 
+static char* errorString(Std_ReturnType error)
+{
+    switch(error)
+    {
+        case E_OK:
+            return "E_OK";
+        break;
+        case E_NOT_OK:
+            return "E_NOT_OK";
+        break;
+        case E_BUSY:
+            return "E_BUSY";
+        break;
+        case QUEUE_FULL:
+            return "QUEUE_FULL";
+        break;
+    }
+}
+
 void GUIInterface_init()
 {
     SecOC_Init(&SecOC_Config);
