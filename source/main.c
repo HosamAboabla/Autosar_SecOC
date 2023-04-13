9bb957dedf80c10b78c00ad520f67addff53154c
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-04-13 02:24:03 +0200

    Returned the old main code

--- source/main.c
+++ source/main.c
@@ -1,50 +1,12 @@
 
 #include "SecOC.h"
 #include "SecOC_Debug.h"
-#include "GUIInterface.h"
 
 int main(void)
 {
-    printf("GUI: started\n");
+    #ifdef DEBUG_ALL
+        SecOC_test();
+    #endif
 
-    GUIInterface_init();
-
-    uint8_t data[] = {9, 9};
-    uint8_t dataLen = sizeof(data);
-
-    printf("GUI: Our data is: %d %d\n", data[0], data[1]);
-
-    //Generate Frame
-    GUIInterface_authenticate(0, data, dataLen);
-
-    uint8_t securedLen = 0;
-    uint8_t* secPdu = GUIInterface_getSecuredPDU(0, &securedLen);
-
-    printf("GUI: Print secured PDU\n");
-    for(int i = 0; i < securedLen; i++)
-    {
-        printf("%d ", secPdu[i]);
-    }
-    printf("\n");
-
-    GUIInterface_alterFreshness(0);
-
-    printf("GUI: Print secured PDU with altered freshness\n");
-    for(int i = 0; i < securedLen; i++)
-    {
-        printf("%d ", secPdu[i]);
-    }
-    printf("\n");
-
-    GUIInterface_alterAuthenticator(0);
-
-    printf("GUI: Print secured PDU with altered authenticator\n");
-    for(int i = 0; i < securedLen; i++)
-    {
-        printf("%d ", secPdu[i]);
-    }
-    printf("\n");
-
-    printf("C Program ran successfully\n");
-    return 0;
+    (void)printf("Program ran successfully\n");
 }
