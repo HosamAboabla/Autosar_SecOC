512eab96c73fdbd4038e36aec51aa80f82072b5f
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-04-13 02:11:46 +0200

    Edited the python program to match the C program

--- source/gui.py
+++ source/gui.py
@@ -4,48 +4,49 @@ lib = ".\\build\\libSecOCLib.dll"
 
 mylib = WinDLL(lib)
 
+print("GUI: started");
+
 # Initializing SecOC
 mylib.GUIInterface_init();
 
 
 # Create an array of bytes in Python
-bytes_array = [9, 9]
-num_bytes = len(bytes_array)
+data = (c_ubyte * 2)(9, 9)
+dataLen = len(data)
 
-# Convert the Python list to a ctypes array of c_uint8 values
-c_bytes_array = (c_uint8 * num_bytes)(*bytes_array)
+print(f"GUI: Our data is: {data[0]} {data[1]}");
 
-mylib.GUIInterface_authenticate(0, c_bytes_array, num_bytes);
+# Generate Frame
+mylib.GUIInterface_authenticate(0, data, dataLen);
 
-securedlen = c_int8()
-secpdu = POINTER(c_uint8)()
+# preparing argument and return type for getsecuredPDU
+securedLen = c_int8()
+secPdu = mylib.GUIInterface_getSecuredPDU(0, byref(securedLen));
 
-secpdu = mylib.GUIInterface_getSecuredPDU(0, byref(securedlen));
+secPdu = cast(secPdu, POINTER(c_uint8))
 
+# Create a a byte array with the returned address
+secPdu = (c_uint8 * securedLen.value).from_address(addressof(secPdu.contents))
 
-secpdu = bytearray(cast(secpdu, POINTER(c_uint8 * securedlen.value)).contents)
-
-for i in range(securedlen.value):
-    print(secpdu[i], end = " ")
+print("GUI: Print secured PDU")
+for i in range(securedLen.value):
+    print(secPdu[i], end = " ")
 print() 
-#print(secpdu_bytes)
+
+mylib.GUIInterface_alterFreshness(0)
+
+print("GUI: Print secured PDU with altered freshness");
+for i in range(securedLen.value):
+    print(secPdu[i], end = " ")
+print() 
 
 
-# for i in range(securedlen.value):
-#     print(secpdu)
-# Get a reference to the function
-#my_function = getattr(mylib, 'SecOC_test')
-mylib.SecOC_test.restype = c_void_p
+mylib.GUIInterface_alterAuthenticator(0)
 
-# Call the function
-result = mylib.SecOC_test(2, 13)
+print("GUI: Print secured PDU with altered authenticator")
+for i in range(securedLen.value):
+    print(secPdu[i], end = " ")
+print()
 
-result = c_char_p(result).value.decode('utf-8')
-
-
-print(result)
-
-print('We still in python')
-
-#getattr(my_functions, 'SecOC_test')()
+print("Python Program ran successfully");
 
--- source/main.c
+++ source/main.c
@@ -10,38 +10,41 @@ int main(void)
     GUIInterface_init();
 
     uint8_t data[] = {9, 9};
-    printf("GUI: Our data is: %d %d\n", data[0], data[1]);
-    //Generate Frame
-    GUIInterface_authenticate(0, data, 2);
+    uint8_t dataLen = sizeof(data);
 
-    uint8_t securedlen = 0;
-    uint8_t* secpdu = GUIInterface_getSecuredPDU(0, &securedlen);
+    printf("GUI: Our data is: %d %d\n", data[0], data[1]);
+
+    //Generate Frame
+    GUIInterface_authenticate(0, data, dataLen);
+
+    uint8_t securedLen = 0;
+    uint8_t* secPdu = GUIInterface_getSecuredPDU(0, &securedLen);
 
     printf("GUI: Print secured PDU\n");
-    for(int i = 0; i < securedlen; i++)
+    for(int i = 0; i < securedLen; i++)
     {
-        printf("%d ", secpdu[i]);
+        printf("%d ", secPdu[i]);
     }
     printf("\n");
 
     GUIInterface_alterFreshness(0);
 
     printf("GUI: Print secured PDU with altered freshness\n");
-    for(int i = 0; i < securedlen; i++)
+    for(int i = 0; i < securedLen; i++)
     {
-        printf("%d ", secpdu[i]);
+        printf("%d ", secPdu[i]);
     }
     printf("\n");
 
     GUIInterface_alterAuthenticator(0);
 
     printf("GUI: Print secured PDU with altered authenticator\n");
-    for(int i = 0; i < securedlen; i++)
+    for(int i = 0; i < securedLen; i++)
     {
-        printf("%d ", secpdu[i]);
+        printf("%d ", secPdu[i]);
     }
     printf("\n");
 
-    (void)printf("Program ran successfully\n");
+    printf("C Program ran successfully\n");
     return 0;
 }
