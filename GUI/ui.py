75bc6c880b497b6416827962eae9d842adcb89f7
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-04-22 02:08:53 +0200

    Made the transmitter the default tab

--- GUI/ui.py
+++ GUI/ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'GUIBheKZF.ui'
+## Form generated from reading UI file 'GUIUqhhzC.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.0
 ##
@@ -289,7 +289,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
 
-        self.ModeTabs.setCurrentIndex(1)
+        self.ModeTabs.setCurrentIndex(0)
         self.pushButton_9.setDefault(False)
 
 
