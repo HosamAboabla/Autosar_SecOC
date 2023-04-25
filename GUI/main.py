ab41c899f3ba1505ee502387abe606eb0351db5e
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-04-25 23:01:19 +0200

    Removed loggers from the main.py

--- GUI/main.py
+++ GUI/main.py
@@ -14,7 +14,6 @@ app = QtWidgets.QApplication(sys.argv)
 
 # Dark Fusion Style
 app.setStyle("Fusion")
-
 dark_palette = QPalette()
 dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
 dark_palette.setColor(QPalette.WindowText, Qt.white)
@@ -31,23 +30,12 @@ dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
 dark_palette.setColor(QPalette.HighlightedText, Qt.black)
 dark_palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
 dark_palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
-
 app.setPalette(dark_palette)
 app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
 
 
+
 dlg = MyDialog()
-print(dlg.gauge.connect)
-# for debugging messages
-#dlg.assingloggers(tlog, rlog)
-
-dlg.tlog.error("Authentication failed")
-dlg.tlog.info("Info message")
-
-dlg.tlog.warning("Authentication stopped")
-dlg.tlog.debug("Debug message")
-dlg.rlog.error("Verification failed")
-
 dlg.show()
 dlg.raise_()
 sys.exit(app.exec_())
