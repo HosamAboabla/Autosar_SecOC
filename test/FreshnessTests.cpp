abc6498f88e05b1991994fb31593b0038d30a18a
HusseinElsaka <ehussein366@gmail.com>
2023-04-27 04:47:22 +0200

    Include only FVM as I TEST it

--- test/FreshnessTests.cpp
+++ test/FreshnessTests.cpp
@@ -2,26 +2,13 @@
 
 extern "C" {    
 
-#include "SecOC_Lcfg.h"
-#include "SecOC_Cfg.h"
-#include "SecOC_PBcfg.h"
-#include "SecOC_Cbk.h"
-#include "ComStack_Types.h"
-#include "Rte_SecOC.h"
-#include "SecOC.h"
-#include "PduR_SecOC.h"
-#include "Csm.h"
-#include "Rte_SecOC_Type.h"
-#include "PduR_Com.h"
-#include "PduR_SecOC.h"
-#include "Pdur_CanTP.h"
-#include "PduR_CanIf.h"
+#include "FVM.h"
+}
 
-#include <string.h>
-extern SecOC_ConfigType SecOC_Config;
-extern const SecOC_TxPduProcessingType     *SecOCTxPduProcessing;
-extern const SecOC_RxPduProcessingType     *SecOCRxPduProcessing;
-extern const SecOC_GeneralType             *SecOCGeneral;
+
+
+
+
 }
 
 
